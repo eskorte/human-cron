7e8f7189380e1cc14b88cd4e791d7083567318e8
Callem Pittard <callem.pittard@gmail.com>
2017-03-23 10:40:23 +1100

    Quick hack - change to english + pass any file in.

--- human-cron.sh
+++ human-cron.sh
@@ -8,13 +8,8 @@
 # licenced GNU/ GPL                            #
 ################################################
 
-if [[ -z $1 ]]; then
-	echo "Podaj uzytkownika CRON, dla ktorego wyswietlic tabele"
-	exit 1;
-fi;
-
-echo -e "Lista komend do wykonania dla uzytkownika $1\n"	
-crontab -l -u $1 | while read line
+echo -e "Cron jobs$1\n"	
+cat $1 | while read line
 	do 
 		if [[ ! $line =~ ^\# && ! -z $line ]]; then
 			COMBINED_DATE=0
@@ -30,27 +25,27 @@ crontab -l -u $1 | while read line
 			WEEK_DAY=`echo "$line" |cut -d" " -f5`
 			
 			#command string
-			ECHO_str="komenda $COMMAND zostanie wykonana"
+			ECHO_str="command: $COMMAND will run"
 			
 			#process values
 				#hours
 				if [[ ! $HOUR =~ ^\*$ && ! -z $HOUR ]]; then
 					if [[ $HOUR =~ ^\*\/[0-9]{1,2}$ && ! -z $HOUR ]]; then
 						NUM_HOUR=`echo "$HOUR" |cut -d"/" -f2`;
-						HR_str="co $NUM_HOUR godzin";
+						HR_str="every $NUM_HOUR hour";
 					elif [[ $HOUR =~ ^[0-9]{1,2}$ && ! -z $HOUR ]]; then
-						HR_str="o godzinie $HOUR"
+						HR_str="$HOUR"
 						COMBINED_DATE=1
 					elif [[ $HOUR =~ ^[0-9]{1,2}-[0-9]{1,2}$ && ! -z $HOUR ]]; then
 						FR_NUM_HOUR=`echo "$HOUR" |cut -d"-" -f1`;
 						TO_NUM_HOUR=`echo "$HOUR" |cut -d"-" -f2`;
-						HR_str="pomiedzy godzina $FR_NUM_HOUR a $TO_NUM_HOUR"				
+						HR_str="between hour $FR_NUM_HOUR and $TO_NUM_HOUR"				
 					elif [[ $HOUR =~ ^[0-9]{1,2}-[0-9]{1,2}\/[0-9]{1,2}$ && ! -z $HOUR ]]; then
 						NUM_HOUR=`echo "$HOUR" |cut -d"/" -f2`;
 						HOUR_RANGE=`echo "$HOUR" |cut -d"/" -f1`;
 						FR_NUM_HOUR=`echo "$HOUR_RANGE" |cut -d"-" -f1`;
 						TO_NUM_HOUR=`echo "$HOUR_RANGE" |cut -d"-" -f2`;
-						HR_str="pomiedzy godzina $FR_NUM_HOUR a $TO_NUM_HOUR, co $NUM_HOUR godziny"
+						HR_str="between hour $FR_NUM_HOUR and $TO_NUM_HOUR, every $NUM_HOUR hours"
 					fi;
 				else
 					HR_str=""
@@ -60,23 +55,23 @@ crontab -l -u $1 | while read line
 				if [[ ! $MINUTES =~ ^\*$ && ! -z $MINUTES ]]; then
 					if [[ $MINUTES =~ ^\*\/[0-9]{1,2}$ && ! -z $MINUTES ]]; then
 						NUM_MINUTES=`echo "$MINUTES" |cut -d"/" -f2`
-						MIN_str="co $NUM_MINUTES minut"
+						MIN_str="every $NUM_MINUTES minutes"
 					elif [[ $COMBINED_DATE == 1 ]]; then
 						if [[ ! $MINUTES =~ ^\*$ && ! -z $MINUTES ]]; then
 							MIN_str="$MINUTES"
 						else
-							MIN_str="w $MINUTES minucie"
+							MIN_str="In $MINUTES minutes"
 						fi;
 					elif [[ $MINUTES =~ ^[0-9]{1,2}-[0-9]{1,2}$ && ! -z $MINUTES ]]; then
 						FR_NUM_MINUTES=`echo "$MINUTES" |cut -d"-" -f1`;
 						TO_NUM_MINUTES=`echo "$MINUTES" |cut -d"-" -f2`;
-						MIN_str="pomiedzy $FR_NUM_MINUTES a $TO_NUM_MINUTES minuta"						
+						MIN_str="between $FR_NUM_MINUTES and $TO_NUM_MINUTES minutes"						
 					elif [[ $MINUTES =~ ^[0-9]{1,2}-[0-9]{1,2}\/[0-9]{1,2}$ && ! -z $MINUTES ]]; then
 						NUM_MINUTES=`echo "$MINUTES" |cut -d"/" -f2`;
 						MINUTES_RANGE=`echo "$MINUTES" |cut -d"/" -f1`;
 						FR_NUM_MINUTES=`echo "$MINUTES_RANGE" |cut -d"-" -f1`;
 						TO_NUM_MINUTES=`echo "$MINUTES_RANGE" |cut -d"-" -f2`;
-						MIN_str="pomiedzy $FR_NUM_MINUTES a $TO_NUM_MINUTES minuta, co $NUM_MINUTES minuty"
+						MIN_str="between $FR_NUM_MINUTES and $TO_NUM_MINUTES minutes, every $NUM_MINUTES minutes"
 					fi;					
 				else
 					MIN_str=""
@@ -86,19 +81,19 @@ crontab -l -u $1 | while read line
 				if [[ ! $MONTH_DAY =~ ^\*$ && ! -z $MONTH_DAY ]]; then
 					if [[ $MONTH_DAY =~ ^\*\/[0-9]{1,2}$ && ! -z $MONTH_DAY ]]; then
 						NUM_MONTH_DAY=`echo "$MONTH_DAY" |cut -d"/" -f2`
-						MON_D_str="co $NUM_MONTH_DAY dni"
+						MON_D_str="every $NUM_MONTH_DAY days"
 					elif [[ $MONTH_DAY =~ ^[0-9]{1,2}$ && ! -z $MONTH_DAY ]]; then
-						MON_D_str="$MONTH_DAY dnia miesiaca"
+						MON_D_str="$MONTH_DAY of the month"
 					elif [[ $MONTH_DAY =~ ^[0-9]{1,2}-[0-9]{1,2}$ && ! -z $MONTH_DAY ]]; then
 						FR_NUM_MONTH_DAY=`echo "$MONTH_DAY" |cut -d"-" -f1`;
 						TO_NUM_MONTH_DAY=`echo "$MONTH_DAY" |cut -d"-" -f2`;
-						MON_D_str="pomiedzy $FR_NUM_MONTH_DAY a $TO_NUM_MONTH_DAY dniem miesiaca"						
+						MON_D_str="between $FR_NUM_MONTH_DAY and $TO_NUM_MONTH_DAY of the month"						
 					elif [[ $MONTH_DAY =~ ^[0-9]{1,2}-[0-9]{1,2}\/[0-9]{1,2}$ && ! -z $MONTH_DAY ]]; then
 						NUM_MONTH_DAY=`echo "$MONTH_DAY" |cut -d"/" -f2`;
 						MONTH_DAY_RANGE=`echo "$MONTH_DAY" |cut -d"/" -f1`;
 						FR_NUM_MONTH_DAY=`echo "$MONTH_DAY_RANGE" |cut -d"-" -f1`;
 						TO_NUM_MONTH_DAY=`echo "$MONTH_DAY_RANGE" |cut -d"-" -f2`;
-						MON_D_str="pomiedzy $FR_NUM_MONTH_DAY a $TO_NUM_MONTH_DAY dniem miesiaca, co $NUM_MONTH_DAY dni"
+						MON_D_str="between $FR_NUM_MONTH_DAY and $TO_NUM_MONTH_DAY of the month, every $NUM_MONTH_DAY days"
 					fi;						
 				else
 					MON_D_str=""
@@ -108,19 +103,19 @@ crontab -l -u $1 | while read line
 				if [[ ! $MONTH =~ ^\*$ && ! -z $MONTH ]]; then
 					if [[ $MONTH =~ ^\*\/[0-9]{1,2}$ && ! -z $MONTH ]]; then
 						NUM_MONTH=`echo "$MONTH" |cut -d"/" -f2`
-						MON_str="co $NUM_MONTH miesiecy"
+						MON_str="every $NUM_MONTH months"
 					elif [[ $MONTH =~ ^[0-9]{1,2}$ && ! -z $MONTH ]]; then
-						MON_str="w $MONTH miesiacu"
+						MON_str="in $MONTH months"
 					elif [[ $MONTH =~ ^[0-9]{1,2}-[0-9]{1,2}$ && ! -z $MONTH ]]; then
 						FR_NUM_MONTH=`echo "$MONTH" |cut -d"-" -f1`;
 						TO_NUM_MONTH=`echo "$MONTH" |cut -d"-" -f2`;
-						MON_str="pomiedzy $FR_NUM_MONTH a $TO_NUM_MONTH miesiacem"						
+						MON_str="between $FR_NUM_MONTH and $TO_NUM_MONTH months"						
 					elif [[ $MONTH =~ ^[0-9]{1,2}-[0-9]{1,2}\/[0-9]{1,2}$ && ! -z $MONTH ]]; then
 						NUM_MONTH=`echo "$MONTH" |cut -d"/" -f2`;
 						MONTH_RANGE=`echo "$MONTH" |cut -d"/" -f1`;
 						FR_NUM_MONTH=`echo "$MONTH_RANGE" |cut -d"-" -f1`;
 						TO_NUM_MONTH=`echo "$MONTH_RANGE" |cut -d"-" -f2`;
-						MON_str="pomiedzy $FR_NUM_MONTH a $TO_NUM_MONTH miesiacem, co $NUM_MONTH miesiecy"
+						MON_str="between $FR_NUM_MONTH and $TO_NUM_MONTH months, every $NUM_MONTH months"
 					fi;						
 				else
 					MON_str=""
@@ -130,19 +125,19 @@ crontab -l -u $1 | while read line
 				if [[ ! $WEEK_DAY =~ ^\*$ && ! -z $WEEK_DAY ]]; then
 					if [[ $WEEK_DAY =~ ^\*\/[0-9]{1,2}$ && ! -z $WEEK_DAY ]]; then
 						NUM_WEEK=`echo "$WEEK_DAY" |cut -d"/" -f2`
-						WEK_str="co $NUM_WEEK dni tygodnia"
+						WEK_str="every $NUM_WEEK day of the week"
 					elif [[ $WEEK_DAY =~ ^[0-9]{1,2}$ && ! -z $WEEK_DAY ]]; then
-						WEK_str="$WEEK_DAY dnia tygodnia"
+						WEK_str="$WEEK_DAY day of the week"
 					elif [[ $WEEK_DAY =~ ^[0-9]{1,2}-[0-9]{1,2}$ && ! -z $WEEK_DAY ]]; then
 						FR_NUM_WEEK_DAY=`echo "$WEEK_DAY" |cut -d"-" -f1`;
 						TO_NUM_WEEK_DAY=`echo "$WEEK_DAY" |cut -d"-" -f2`;
-						WEK_str="pomiedzy $FR_NUM_WEEK_DAY a $TO_NUM_WEEK_DAY dniem tygodnia"						
+						WEK_str="between $FR_NUM_WEEK_DAY and $TO_NUM_WEEK_DAY day of the week"						
 					elif [[ $WEEK_DAY =~ ^[0-9]{1,2}-[0-9]{1,2}\/[0-9]{1,2}$ && ! -z $WEEK_DAY ]]; then
 						NUM_WEEK_DAY=`echo "$WEEK_DAY" |cut -d"/" -f2`;
 						WEEK_DAY_RANGE=`echo "$WEEK_DAY" |cut -d"/" -f1`;
 						FR_NUM_WEEK_DAY=`echo "$WEEK_DAY_RANGE" |cut -d"-" -f1`;
 						TO_NUM_WEEK_DAY=`echo "$WEEK_DAY_RANGE" |cut -d"-" -f2`;
-						MON_str="pomiedzy $FR_NUM_WEEK_DAY a $TO_NUM_WEEK_DAY dniem tygodnia, co $NUM_WEEK_DAY dni"
+						MON_str="between $FR_NUM_WEEK_DAY and $TO_NUM_WEEK_DAY day of the week, every $NUM_WEEK_DAY days"
 					fi;						
 				else
 					WEK_str=""
@@ -173,10 +168,10 @@ crontab -l -u $1 | while read line
 					DATE_str="$DATE_str$MON_D_str, "
 				fi;					
 			else
-				DATE_str="codziennie"
+				DATE_str="daily"
 			fi;	
 							
-			if [[ $DATE_str =~ ^codziennie$ ]]; then
+			if [[ $DATE_str =~ ^daily$ ]]; then
 				echo "-+ $ECHO_str $DATE_str $TIME_str" | sed 's/.\{2\}$//'
 			else				
 				echo "-+ $ECHO_str $TIME_str $DATE_str" | sed 's/.\{2\}$//'
